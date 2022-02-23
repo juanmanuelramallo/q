90ca9d2ceb862105791e0c63802e6b7c0adf731c
Juan Manuel Ramallo <juanmanuelramallo@hey.com>
2022-02-22 22:02:48 -0300

    Fix playerNameUniqueness
    Added jest to have simple tests.

--- src/playerNameUniqueness.js
+++ src/playerNameUniqueness.js
@@ -3,8 +3,9 @@ import { room } from "./room";
 function playerNameUniqueness(player) {
   var players = room.getPlayerList();
   var playerNames = players.map(p => p.name);
-  var deleted = playerNames.splice(0, 2, player.name);
-  if (deleted.length == 2) {
+  playerNames.splice(playerNames.indexOf(player.name), 1);
+
+  if (playerNames.indexOf(player.name) !== -1) {
     room.kickPlayer(player.id, "Ya ahi alguien con ese nombre conectado", false);
     return false;
   }
