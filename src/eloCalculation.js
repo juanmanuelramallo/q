03aab6a3302fac98a95db7b1e0f656cc91cc4a54
Danilo Garcia <dani24gl@gmail.com>
2022-02-23 18:42:07 -0300

    [Elo Calculation] Addressing PR comments

--- src/eloCalculation.js
+++ src/eloCalculation.js
@@ -1,14 +1,14 @@
 
 function calculateNewEloDelta(teamPlayers, hasWon, opponentTeamPlayers) {
-  var teamElo = totalTeamElo(teamPlayers);
-  var opponentTeamElo = totalTeamElo(opponentTeamPlayers);
+  const teamElo = totalTeamElo(teamPlayers);
+  const opponentTeamElo = totalTeamElo(opponentTeamPlayers);
 
   return newEloDelta(teamElo, hasWon, opponentTeamElo);
 }
 
 function totalTeamElo(players) {
   return players.reduce(function(total, player) {
-    var playerElo = Object.values(player)[0].elo;
+    const playerElo = Object.values(player)[0].elo;
 
     return total + playerElo;
   }, 0);
@@ -16,8 +16,8 @@ function totalTeamElo(players) {
 
 function newEloDelta(totalTeamElo, hasWon, totalOpponentTeamElo) {
   const k = 30;
-  let finalScore = (hasWon ? 1 : 0);
-  let expectedScore = 1 / (1 + 10**((totalOpponentTeamElo - totalTeamElo) / 400));
+  const finalScore = (hasWon ? 1 : 0);
+  const expectedScore = 1 / (1 + 10**((totalOpponentTeamElo - totalTeamElo) / 400));
 
   return Math.round((finalScore - expectedScore) * k);
 }
