d38e036eb96bb74ec368a5d120a163be9269c66c
Juan Manuel Ramallo <juanmanuelramallo@hey.com>
2022-02-26 17:03:31 -0300

    Renamed recording files

--- src/recording.js
+++ src/recording.js
@@ -1,8 +1,13 @@
 import { downloadFile } from './downloadFile';
+import { getRedPlayers, getBluePlayers } from './players';
+import { sanitize } from "./utils";
 
 function handleStopRecording(recording) {
-  var today = new Date();
-  var filename = "Recording " + today.toDateString() + " " + today.toLocaleTimeString() + ".hbr2";
+  var filename = [Date.now()];
+  filename = filename.concat(getRedPlayers().map(player => sanitize(player.name)));
+  filename = filename.concat('vs');
+  filename = filename.concat(getBluePlayers().map(player => sanitize(player.name)));
+  filename = filename.join('-') + '.hbr2';
   downloadFile(filename, recording);
   // TODO: Perhaps send it to a s3-like service
 }
--- src/utils.js
+++ src/utils.js
@@ -6,4 +6,18 @@ function randomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-export { randomInt }
+/* Removes dots, emojis and other non-alphanumeric characters from a string.
+ *
+ * @param name {string} - The string to sanitize
+ * @return {string} - The sanitized string. Returns "sanitized" if the output is empty after the sanitization. */
+function sanitize(name) {
+  var sanitized = name.replace(/[^a-zA-Z0-9]/g, '');
+
+  if (sanitized.length === 0) {
+    return 'sanitized';
+  }
+
+  return sanitized;
+}
+
+export { randomInt, sanitize }
